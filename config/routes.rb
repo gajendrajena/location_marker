62694f315994312ffe927f30cb1762d470cd0c40
Gajendra Jena <gaju.mca@gmail.com>
2019-04-05 00:21:57 +0530

    Added API scope and versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api do
-    post 'user/token' => 'user_token#create'
-    post 'user/current' => 'users#current'
+  namespace :api, defaults: {format: :json} do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      post 'user/token' => 'user_token#create'
+      post 'user/current' => 'users#current'
+      resources :locations
+    end
   end
 end
