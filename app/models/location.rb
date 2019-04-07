57f89b9b7fb962ea4066149fb2a21c48b72a470c
Gajendra Jena <gaju.mca@gmail.com>
2019-04-07 21:17:19 +0530

    Add geocoder gem for latitude and longitude for a location
     - added geocoder gem
     - added geocoder config by running
     `rails g geocoder:config`
     - seed few locations
     - format location to send json data required by client
     - added a fav icon to supress api error

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,17 @@
 class Location < ApplicationRecord
+  geocoded_by :address
+
+  after_validation :geocode
+
+  validates :address, presence: true
+
+  def as_json(options={})
+    {
+      pos: {lat: self.latitude, lng: self.longitude},
+      id: self.id,
+      address: address,
+      latitude: latitude,
+      longitude: longitude
+    }
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
 addresses = [
-  {address: 'Europa-Allee 50; 60327 Frankfurt am Main, Germany'},
-  {address: 'Hohenzollernring 56, 48145 Münster, Germany'},
-  {address: 'Vogelsanger Weg 80; 40470 Düsseldorf, Germany'},
-  {address: 'Dernburgstr. 50; 14057 Berlin, Germany'},
-  {address: 'Bonner Talweg 100; D-53113 Bonn'},
-  {address: 'Nauheimer Str. 100; 70372 Stuttgart, Germany '}
+  {address: 'Leipzig'},
+  {address: 'Checkpoint Charlie'}
 ]
 
 Location.create(addresses)
